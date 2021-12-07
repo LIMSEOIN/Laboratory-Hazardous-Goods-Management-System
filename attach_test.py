a8c110088ee090acb4909c9293d731fd6e8b4b4c
LIMSEOIN <62792181+LIMSEOIN@users.noreply.github.com>
2021-12-07 15:11:25 +0900

    Update attach_test.py

--- attach_test.py
+++ attach_test.py
@@ -4,10 +4,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 smtp = smtplib.SMTP('smtp.gmail.com',587)
 smtp.starttls()
-smtp.login('test20184468@gmail.com','vdmg znzw cspv yjeo')
+smtp.login('??','???')
 msg = MIMEMultipart()
 msg['Subject'] = 'dfdfdfdfdf'
-msg['To'] = 'gjw04054@naver.com'
+msg['To'] = '???'
 text = MIMEText('asdfgda')
 msg.attach(text)
 file_name = 'test.jpg'
@@ -15,5 +15,5 @@ with open(file_name,'rb') as file_FD:
     etcPart = MIMEApplication(file_FD.read())
     etcPart.add_header('Content-Disposition','attachment', filename=file_name)
     msg.attach(etcPart)
-    smtp.sendmail('test20184468@gmail.com','gjw04054@naver.com',msg.as_string())
+    smtp.sendmail('???','???',msg.as_string())
 smtp.quit()
